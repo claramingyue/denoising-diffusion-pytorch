30d24f223b98125a6fbefb316fc2469d7c72cada
claramingyue <124404447+claramingyue@users.noreply.github.com>
2025-01-21 13:32:16 +0100

    kernel_size changed to 5x5

--- denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
+++ denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
@@ -88,10 +88,16 @@ def unnormalize_to_zero_to_one(t):
 
 # small helper modules
 
-def Upsample(dim, dim_out = None):
+# def Upsample(dim, dim_out = None):
+#     return nn.Sequential(
+#         nn.Upsample(scale_factor = 2, mode = 'nearest'),
+#         nn.Conv2d(dim, default(dim_out, dim), 5 , padding = 5//2)
+#     )
+
+def Upsample(dim, dim_out = None, kernel_size=5):
     return nn.Sequential(
-        nn.Upsample(scale_factor = 2, mode = 'nearest'),
-        nn.Conv2d(dim, default(dim_out, dim), 3, padding = 1)
+        nn.Upsample(scale_factor = 2, mode = 'nearest'),  # Duplica la resolución
+        nn.Conv2d(dim, default(dim_out, dim), kernel_size, padding = kernel_size // 2)  # Filtro 5x5
     )
 
 def Downsample(dim, dim_out = None):
